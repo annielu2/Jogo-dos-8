d85afcc00b3739eddf50fedd6f51529a7048083b
annielu2 <aluizacalixto@gmail.com>
2022-07-17 16:25:49 -0300

    Interacao mouse + pecas

--- Reversi.py
+++ Reversi.py
@@ -9,6 +9,7 @@ pygame.display.set_caption('Reversi')
 pos = pygame.image.load(r'Sprites/Posicao.png')
 peca_branca = pygame.image.load(r'Sprites/Peca_branca.png')
 posicoes = []
+estado = {}
 
 
 for i in range(8):
@@ -16,11 +17,13 @@ for i in range(8):
        x = 220 + i*75
        y = 50 + j*75
        aux = (x, y)
-       posicoes.append(aux) 
+       posicoes.append(aux)
+       estado[aux] = 'VAZIO'
        
+             
 while True:
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == QUIT:	
             pygame.quit()
         elif event.type == MOUSEBUTTONDOWN:
             X, Y = pygame.mouse.get_pos()
@@ -29,14 +32,17 @@ while True:
             	j = floor((Y-50)/75)
             	auxX = 220 + i*75
             	auxY = 50 + j*75
-            	print(auxX, auxY)
+            	estado[(auxX, auxY)] = 'WHITE'
+            
             	
-            	
-        
     
+    list_pos = estado.keys()
     screen.fill((225,225,225))
-    for posicao in posicoes:  
+    for posicao in list_pos:  
     	screen.blit(pos, posicao)
+    	if estado[posicao] == 'WHITE':
+    	    screen.blit(peca_branca, posicao)
     pygame.display.update()    
-	
+
+
 
