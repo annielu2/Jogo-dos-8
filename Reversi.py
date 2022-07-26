c5dd3d146dcae223234b36363f29f8e2b18f8b1c
annielu2 <aluizacalixto@gmail.com>
2022-07-26 03:16:11 -0300

    Finalizando tela inicial e parte do game over

--- Reversi.py
+++ Reversi.py
@@ -12,6 +12,7 @@ screen =  pygame.display.set_mode((1000, 750))
 pygame.display.set_caption('Reversi')
 
 backgroundReversi = pygame.image.load(r'Sprites/wood.jpg')
+logo = pygame.image.load(r'Sprites/logo.png')
 
 pos = pygame.image.load(r'Sprites/Posicao.png')
 peca_branca = pygame.image.load(r'Sprites/Peca_branca.png')
@@ -19,12 +20,13 @@ peca_preta = pygame.image.load(r'Sprites/Peca_preta.png')
 borda = pygame.image.load(r'Sprites/Borda.png')
 possibilidades = pygame.image.load(r'Sprites/Possibilidade.png')
 
-font = pygame.font.SysFont('Arial', 24, bold = pygame.font.Font.bold)
+font = pygame.font.Font(r'Font/BD_Cartoon_Shout.ttf', 24)
+font_placar = pygame.font.Font(r'Font/BD_Cartoon_Shout.ttf', 50)
 
-botao_jogar = pygame.image.load(r'Sprites/Botao.png')
-botao_jogar_click = pygame.image.load(r'Sprites/Botao2.png')
-reversi_escrita = pygame.image.load(r'Sprites/REVERSI.png')
-
+botao_jogar = pygame.image.load(r'Sprites/Botao.png').convert_alpha()
+botao_jogar_click = pygame.image.load(r'Sprites/Botao2.png').convert_alpha()
+botao_jogar_novamente = pygame.image.load(r'Sprites/novo_jogo.png').convert_alpha()
+botao_sair = pygame.image.load(r'Sprites/Sair.png').convert_alpha()
 
 def background_display(game):
     screen.fill((225,225,225))
@@ -73,24 +75,43 @@ def display(game, todasPoss = []):
         passar = "Nenhuma jogada possível! Clicke para passar a jogada."
         txt = font.render(passar, 1, (145, 0, 0))
         screen.blit(txt,(50, 700))
-    
-    """elif(game.estado == "FIN"):
-        time.sleep(2.0)
-        game_over()"""
     	
     pygame.display.update() 
 
 
-def game_over():
+def game_over(game):
+    screen.blit(backgroundReversi, (0, 0))
+
+    screen.blit(botao_jogar_novamente, (275, 350))
+    screen.blit(botao_sair, (275, 500))
+
+    screen.blit(peca_branca, (275, 250))
+    screen.blit(peca_preta, (500, 250))
+
+    placarPretas = str(game.placar["BLACK"])
+    txt = font_placar.render(placarPretas, 1, (20, 20, 20))
+    screen.blit(txt,(600, 262)) 
+    
+    placarBrancas = str(game.placar["WHITE"])
+    txt = font_placar.render(placarBrancas, 1, (20, 20, 20))
+    screen.blit(txt,(375, 262))
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:  
                 pygame.quit()
             elif event.type == MOUSEBUTTONDOWN:
-                new_game = ReversiGame.ReversiGame()
-                reversiInterface(new_game)
-        screen.fill((225, 225, 225))
+                X, Y = pygame.mouse.get_pos()
+                if (X >= 275 and X <= 725 and Y >= 350 and Y <= 450):               
+                    new_game = ReversiGame.ReversiGame()
+                    time.sleep(0.2)
+                    reversiInterface(new_game)
+                elif (X >= 275 and X <= 725 and Y >= 500 and Y <= 600):
+                    pygame.quit()
+                    
+                
         pygame.display.update()
+
                 
 def reversiInterface(game):
     display(game, game.getTodasPoss())
@@ -111,24 +132,29 @@ def reversiInterface(game):
                         game.jogar(jogadaAdv[0], jogadaAdv[1])
                         time.sleep(0.5)
                         display(game, game.getTodasPoss())
+                
+            if(game.estado == "FIN"):
+                time.sleep(2.0)
+                game_over(game)
+
                     
 
 def tela_inicial(): 
     game = ReversiGame.ReversiGame()
     screen.blit(backgroundReversi, (0, 0))
-    screen.blit(reversi_escrita, (280, 175))
+    screen.blit(botao_jogar, (375, 400))
+    screen.blit(logo, (250, 100))
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
             elif event.type == MOUSEBUTTONDOWN:
                 X, Y = pygame.mouse.get_pos()
-                if (X >= 375 and X <= 625 and Y >= 332 and Y <= 419):               
-                    screen.blit(botao_jogar_click, (375, 332))
+                if (X >= 350 and X <= 650 and Y >= 400 and Y <= 550):               
+                    #screen.blit(botao_jogar_click, (375, 400))
                     pygame.display.update()
                     time.sleep(0.2)
                     reversiInterface(game)
-        screen.blit(botao_jogar, (375, 332))
         pygame.display.update()
 
 tela_inicial()
